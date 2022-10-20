087b72a711f6810cca6d5384f24bee2aa0885f6f
Ahmad Alakash <ahmadalakash000@gmail.com>
2022-10-20 23:00:30 +0200

    create folders

--- organziar.py
+++ organziar.py
@@ -1,20 +1,31 @@
 
 # Automation 
 
-import os   # function to display what have we in one path 
+import os
+import shutil   # function to display what have we in one path 
 
 folder_path = 'project1'
+os.chdir(folder_path)
+os.mkdir('imges')  # create folder with use :os.mkdir()
+os.mkdir('docs')  # create folder with use :os.mkdir()
+os.mkdir('audio_vedio')  # create folder with use :os.mkdir()
 
 
 
-for file in os.listdir(path=folder_path):
+for file in os.listdir('.'):
     # now nedd to see the file has wich end jpg or mp3 or any end from this >> search in google
     # get file extention
     filename,file_extention = os.path.splitext(file)
-
-    if file_extention in ['.jpg', '.png', '.gif', '.jpeg' ] :
-        print (file)
-
-
    
+    if file_extention in ['.jpg', '.png', '.gif', '.jpeg' ] :
+        shutil.move(file,'imges')
+
+    if file_extention in ['.pdf', '.epub', '.word', '.docs']:
+        shutil.move(file, 'docs')
+        
+    if file_extention in ['.mp3', '.mp4', '.avi',  ] :
+        shutil.move(file, 'audio_vedio')
+        
+
+
    
