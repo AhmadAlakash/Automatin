5d64b55fe76e9b28c760df365bfb499e1d3e1cfe
Ahmad Alakash <ahmadalakash000@gmail.com>
2022-10-22 00:15:58 +0200

    add a new style code

--- organziar.py
+++ organziar.py
@@ -1,11 +1,12 @@
 
+
 # Automation 
 
 from distutils import extension
 import os
 import shutil   # function to display what have we in one path 
 
-'''folder_path = 'project2'
+folder_path = 'project2'
 os.chdir(folder_path)
 os.mkdir('imges')  # create folder with use :os.mkdir()
 os.mkdir('docs')  # create folder with use :os.mkdir()
@@ -16,9 +17,9 @@ os.mkdir('rar+zip')  # create folder with use :os.mkdir()
    # to know what from file we have can we this code using ' 
    # extention []
    # for file in os.listdir('.'):
-#         filename,file_extentions = os.path.spiltext(file)
-   # '          extentions.append(file_extentions)
-   # print (extentions)
+#         filename,file_extension = os.path.spiltext(file)
+   # '          extension.append(file_extension)
+   # print (extension)
 
 
 
@@ -31,32 +32,32 @@ for file in os.listdir('.'):
     if file_extention in ['.jpg', '.png', '.gif', '.jpeg' ] :
         shutil.move(file,'imges')
 
-    if file_extention in ['.pdf', '.epub', '.word', '.docs']:
+    elif file_extention in ['.pdf', '.epub', '.word', '.docs']:
         shutil.move(file, 'docs')
         
-    if file_extention in ['.mp3', '.mp4', '.avi',  ] :
+    elif file_extention in ['.mp3', '.mp4', '.avi',  ] :
         shutil.move(file, 'audio_vedio')
         
 
         
-    if file_extention in ['.rar', '.zip'  ] :
+    elif file_extention in ['.rar', '.zip'  ] :
         shutil.move(file, 'rar+zip')
         
 
         
-    if file_extention in ['.exe' ] :
+    elif file_extention in ['.exe' ] :
         shutil.move(file, 'exe')
 
 
 print ('done')
-'''
+
 
 # to make the code more good we can 'istead of  ' file_extention in ' this write endswith but he take ()'
 
 
 # new style from this code :
 
-
+'''
 def working_dir(path):
     os.chdir(path)
     print(f'moved to : {path}')
@@ -68,39 +69,52 @@ def check_folder():
         if not os.path.exists(folder):
             os.mkdir(folder)
 
-def get_extentions():
+def get_extension():
     for file in os.listdir('.'):
-        filename,file_extention = os.path.splitext(file)
-        if file_extention not in extentions:
-            extentions.append(file_extention)
-    print(extentions)
+        filename, file_extension = os.path.splitext(file)
+        if file_extension not in extension:
+            extension.append(file_extension)
+    print(f'Avaliable Extention {extension}')
     pass
 
 
 def organize():
+    print('Start organizing Files')
     for file in os.listdir('.'):
-        if file.endswith(('.jpg', '.png', '.gif', '.jpeg')):
-            shutil.move(file,'imges')
-    
+        # now nedd to see the file has wich end jpg or mp3 or any end from this >> search in google
+        # get file extention
+   
+        if file.endswith in ['.jpg', '.png', '.gif', '.jpeg']:
+            shutil.move(file, 'imges')
 
-        if file.endswith(('.mp4', '.avi')):
-            shutil.move(file,'videos')
-    
+        elif file.endswith in (('.pdf', '.epub', '.word', '.docs')):
+            shutil.move(file, 'docs')
 
-        if file.endswith(('.pdf', '.zip','.rar','.exe','.text','.word')):
-            shutil.move(file,'compressed')
-    
+        elif file.endswith in (('.mp3', '.mp4', '.avi')):
+            shutil.move(file, 'audio_vedio')
 
+        elif file.endswith in (('.rar', '.zip')):
+            shutil.move(file, 'rar+zip')
 
+        elif file.endswith in (('.exe')):
+            shutil.move(file, 'exe')
+    print('Finish Organizing Files')
 
+  
 def main ():
-    working_path= input ('enter path :')
+    working_path = input ('Enter path :')
+    print ('Start Working...')
+    working_dir(working_path)
     check_folder()
+    get_extension()
+    organize()
+    
 
 
 
 
-if __name__=='__main__':
+if __name__ =='__main__':
     main()
+'''
 
    
